d4dd61337440c9e02e5a0e862212881017dc8a57
gy <228713680@qq.com>
2019-03-28 23:02:07 +0800

    搜索组件开发

---
+++ src/components/Search.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="search">
+    <el-input class="input" v-model="keyword" :placeholder="placeholderText" clearable></el-input>
+    <el-button type="primary" @click="search">搜索</el-button>
+    <el-button type="primary" @click="exportExcel" v-if="isNeedExportBtn">导出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data () {
+    return {
+      keyword: ''
+    }
+  },
+  props: {
+    placeholderText: {
+      type: String,
+      default: '请输入'
+    },
+    isNeedExportBtn: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    // 点击搜索按钮
+    search () {
+      this.$emit('search', this.keyword)
+    },
+    // 点击导出按钮
+    exportExcel () {
+      this.$emit('exportExcel', this.keyword)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.search {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 20px 0;
+  .input {
+    width: 320px;
+    margin-right: 10px;
+  }
+}
+</style>
