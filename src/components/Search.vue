81856ace272bc8c4a7caed924c1629b26bd22bd4
gy <228713680@qq.com>
2019-03-30 22:51:46 +0800

    搜索组件添加新增按钮

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,6 +3,7 @@
     <el-input class="input" v-model="keyword" :placeholder="placeholderText" clearable></el-input>
     <el-button type="primary" @click="search">搜索</el-button>
     <el-button type="primary" @click="exportExcel" v-if="isNeedExportBtn">导出</el-button>
+    <el-button type="primary" @click="addUser" v-if="isNeedAddBtn">新增</el-button>
   </div>
 </template>
 
@@ -22,6 +23,10 @@ export default {
     isNeedExportBtn: {
       type: Boolean,
       default: false
+    },
+    isNeedAddBtn: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
@@ -32,6 +37,10 @@ export default {
     // 点击导出按钮
     exportExcel () {
       this.$emit('exportExcel', this.keyword)
+    },
+    // 点击新增
+    addUser () {
+      this.$emit('add')
     }
   }
 }
